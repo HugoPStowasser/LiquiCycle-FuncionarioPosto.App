b1f3705d40316fe1fef88858640eb70c4c35f3c2
Hugo Stowasser <hugopstowasser@gmail.com>
2023-06-14 22:19:23 -0300

    LoginView

--- Views/Account/LoginView.xaml.cs
+++ Views/Account/LoginView.xaml.cs
@@ -2,8 +2,23 @@ namespace LiquiCycle_FuncionarioPosto.Views.Account;
 
 public partial class LoginView : ContentPage
 {
-	public LoginView()
-	{
-		InitializeComponent();
-	}
+    public LoginView()
+    {
+        InitializeComponent();
+
+    }
+    private void LoginPost(object sender, EventArgs e)
+    {
+        Application.Current.MainPage = new NavigationPage(new AppView());
+    }
+
+    private void NavigationForgotPassword(object sender, EventArgs e)
+    {
+        Navigation.PushModalAsync(new ForgotPasswordView());
+    }
+
+    private void NavigationCreateUser(object sender, EventArgs e)
+    {
+        Navigation.PushModalAsync(new CreateUserView());
+    }
 }
