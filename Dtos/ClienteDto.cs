b2f4255219f11f52ff6aae5869b3f8feb0b6b628
Hugo Stowasser <hugopstowasser@gmail.com>
2023-07-08 20:04:57 -0300

    More Dto's

---
+++ Dtos/ClienteDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiquiCycle_FuncionarioPosto.Dtos
+{
+    public class ClienteDto
+    {
+        public int Id { get; set; }
+        public double Saldo { get; set; }
+        public UsuarioDto Usuario { get; set; }
+    }
+}
---
+++ Dtos/LoginDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiquiCycle_FuncionarioPosto.Dtos
+{
+    public class LoginDto
+    {
+        public AdministradorDto Administrador { get; set; }
+        public ClienteDto Cliente { get; set; }
+        public FuncionarioPostoDto FuncionarioPosto { get; set; }
+        public DonoPostoDto DonoPosto { get; set; }
+    }
+}
