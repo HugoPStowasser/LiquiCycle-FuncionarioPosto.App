a4d301bfce70635768e528eb36a138e531b5a7bc
Gabriel Portela Mello <gabrielportelamello@gmail.com>
2023-07-09 05:32:59 -0300

    Criação da tela de usina e algumas modais como telas separadas para otimizar tempo de prod

--- Views/App/UsinasView.xaml.cs
+++ Views/App/UsinasView.xaml.cs
@@ -7,29 +7,12 @@ public partial class UsinasView : ContentPage
 	public UsinasView()
 	{
 		InitializeComponent();
-        List<getUsinas> usinas = new List<getUsinas>()
-        {
-            new getUsinas(){nome = "Posto Casa Verde", codigo = "#34567"},
-            
-
-        };
-        listUsinas.ItemsSource = usinas;
+        
     }
 
-    private void PickerSelectedIndexChanged(object sender, EventArgs e)
+    private void enviaUsina(object sender, EventArgs e)
     {
-        var picker = (Picker)sender;
-        var selectedOption = (string)picker.SelectedItem;
-
-        Console.WriteLine($"Opção selecionada: {selectedOption}");
-    }
-
-    private void limpaTudo(object sender, TappedEventArgs e) { }
-
-    public class getUsinas
-    {
-        public string nome { get; set; }
-        public string codigo { get; set; }
+        Navigation.PushModalAsync(new EnviarUsina());
     }
 
 }
